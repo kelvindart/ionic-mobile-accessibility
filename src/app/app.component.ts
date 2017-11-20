777d5c0c8549342985f2848eb13a80bea70dbac8
Kelvin Dart <kelvin.dart@crimsontide.co.uk>
2017-11-20 10:52:37 +0000

    feat: implement basic mobile-accessibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,32 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
+import { MobileAccessibility } from '@ionic-native/mobile-accessibility';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+declare const window: any;
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    mobileAccessibility: MobileAccessibility,
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      mobileAccessibility.usePreferredTextZoom(true);
     });
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular/platform/platform';
+
+import { MobileAccessibility } from '@ionic-native/mobile-accessibility';
+
+declare const window: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public zoomLevel: number = 100;
 
+  constructor(
+    public navCtrl: NavController,
+    public platform: Platform,
+    public mobileAccessibility: MobileAccessibility
+  ) {
+    this.platform.ready().then(() => {
+      this.mobileAccessibility.getTextZoom().then(e => this.zoomLevel = e);
+    })
+  }
+
+  updateZoom() {
+    this.mobileAccessibility.setTextZoom(this.zoomLevel);
   }
 
 }
